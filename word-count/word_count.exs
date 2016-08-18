91703c9540eb12a9cc946408a5a998f484ffcd03
Eleni Lixourioti <contact@eleni.co>
2016-08-18 21:24:42 +0100

    Add word-cound exercise

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,6 +6,12 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
+    # Remove punctuation and split the words
+    words = Regex.replace(~r/[^\p{L}\p{N}\-]/u, sentence, " ") |> String.split
 
+    # Build the counter map
+    words |> Enum.reduce(%{}, fn(word, acc) ->
+      Map.update(acc, String.downcase(word), 1, &(&1 + 1))
+    end)
   end
 end
